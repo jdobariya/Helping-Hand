7d8e90ac4778ca4d6c1ab2bcebb00ecd2b8d3d22
Will Liu <will65536@gmail.com>
2023-04-11 22:35:46 -0400

    add some settings and functions about events and set events index, then add package-lock.json to gitignore

--- data/events.js
+++ data/events.js
@@ -1,1 +1,57 @@
-import {events} from '../config/mongoCollections.js'
+import {events} from '../config/mongoCollections.js';
+import { ObjectId } from 'mongodb';
+import * as validation from '../validation.js'; 
+
+const exportedMethods = {
+  async addEvent (
+    event_name,
+    description,
+    date,
+    time,
+    location,
+    hostInfo,
+    tags,
+  ){
+
+  },
+
+  async removeEvent(id) {
+
+  },
+
+  async updateEventPatch(id, updatedEvent) {
+
+  },
+
+  // will return all app's events
+  async getAllAppEvents() {
+
+  },
+
+  // will return all app's events which have all the tags(Array)
+  async getAllAppEventsWithTags(tags) {
+
+  },
+  
+  async getAllAppEvents() {
+
+  },
+
+  async getAllUserHostEvents() {
+
+  },
+
+  async getAllUserParticipateEvents() {
+
+  },
+
+  async getEventById(id) {
+
+  },
+
+  async getEventsByTag() {
+
+  }
+};
+
+export default exportedMethods;
