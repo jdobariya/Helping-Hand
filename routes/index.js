747a30ae645e468e0305c0206282947ca1e69d72
jdobariya <jay.deep3210@gmail.com>
2023-04-19 16:30:49 -0400

    Created routes for events, about and event pages. Created about page.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
-import loginRoutes from './login.js'
+import loginRoutes from "./login.js";
+import eventsRoutes from "./events.js";
+import aboutRoutes from "./about.js";
 
 const constructorMethod = (app) => {
-    app.use('/home', (req, res) => {
-        res.render('homepage', {
-            title: "Helping Hands"
-        })
-    })
-    app.use('/login', loginRoutes)
+  app.use("/home", (req, res) => {
+    res.render("homepage", {
+      title: "Helping Hands",
+    });
+  });
+  app.use("/login", loginRoutes);
+  app.use("/events", eventsRoutes);
+  app.use("/about", aboutRoutes);
 
-    app.use('*', (req, res) => {
-        res.redirect('/home')
-    })
-}
+  app.use("*", (req, res) => {
+    res.send({ Error: "Resource not found" });
+  });
+};
 
 export default constructorMethod;
