b8ef57cbbe53fe5f9528ee8bb9f8e17c49be176d
jdobariya <jay.deep3210@gmail.com>
2023-04-19 22:09:32 -0400

    Changes to login form

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,22 @@
-function onSubmitClicked(){
-  let email = document.getElementById('email');
-  let password = document.getElementById('password');
-  let errorMessage = document.getElementById('error_message');
+function onSubmitClicked(event) {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  let errorMessage = document.getElementById("error_message");
 
-  console.log('Form submission fired');
-  console.log('Has a form');
+  console.log("Form submission fired");
+  console.log("Has a form");
   event.preventDefault();
 
   if (!email.value.trim()) {
-    email.value = '';
+    email.value = "";
     errorMessage.hidden = false;
-    errorMessage.innerHTML = 'Please enter an email';
+    errorMessage.innerHTML = "Please enter an email";
   } else if (!password.value.trim()) {
-    password.value = '';
+    password.value = "";
     errorMessage.hidden = false;
-    errorMessage.innerHTML = 'Please enter a password';
-  }
-  else {
+    errorMessage.innerHTML = "Please enter a password";
+  } else {
     errorMessage.hidden = true;
-    document.getElementById('login_form').submit();
+    document.getElementById("login_form").submit();
   }
 }
--- routes/login.js
+++ routes/login.js
@@ -1,21 +1,21 @@
-import {Router} from "express";
+import { Router } from "express";
 import {} from "../validation.js";
 
 const router = Router();
 
 router.route("/").get((req, res) => {
-    res.render('login', {
-        title: 'Login'
-    })
-})
+  res.render("login", {
+    title: "Login",
+  });
+});
 
 router.route("/").post((req, res) => {
-    let body = req.body;
-    let username = body.username;
-    let password = body.password;
+  let body = req.body;
+  let username = body.email;
+  let password = body.password;
 
-    console.log(username, password)
-    res.redirect('/home')
-})
+  console.log(username, password);
+  res.redirect("/home");
+});
 
-export default router
+export default router;
