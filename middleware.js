9647ac4c12e6e431d2c43b7941fefa4217d9b49a
JayTalekar <jaytalekar.developer@gmail.com>
2023-04-26 15:39:50 -0400

    Login implemented with express sessions

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 export function logger(req, res, next) {
     let userRole = 'Non-Authenticated User';
-    if (req.session && req.session.user && req.session.user.role) {
+    if (req.session && req.session.loggedIn) {
         userRole = 'Authenticated User';
     }
     console.log(`[${new Date().toUTCString()}]: ${req.method} ${req.originalUrl} (${userRole})`);
@@ -9,3 +9,20 @@ export function logger(req, res, next) {
 
 
 // Add any middleware functions you need here.
+export function redirectLogin(req, res, next) {
+    if (req.session && req.session.loggedIn) res.redirect('/home');
+    else next();
+}
+
+export function redirectSignup(req, res, next) {
+    if (req.session && req.session.loggedIn) res.redirect('/home');
+    else next();
+}
+
+// export function redirectLogout(req, res, next){
+//     if (req.session && req.session.loggedIn) {
+//         next();
+//     }else{
+//         res.redirect('/login');
+//     }
+// }
