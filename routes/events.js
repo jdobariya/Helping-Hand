01a80a1403a098d78cc1e1ed64106fbd6e6f0b4f
jdobariya <jay.deep3210@gmail.com>
2023-04-28 21:37:41 -0400

    Added events page

--- routes/events.js
+++ routes/events.js
@@ -1,17 +1,47 @@
 import { Router } from "express";
 import {} from "../validation.js";
+import { eventData } from "../data/index.js";
 
 const router = Router();
 
-router.route("/").get((req, res) => {
-  if(req.session &&req.session.loggedIn)
-    {
-      return res.render('events',{title:"Browse Events",user:true})
-     }
-    else
-    {
-    return  res.render('events',{title:"Browse Events",user:false})
+function getEventsInRow(events) {
+  let eventRows = [];
+  let eventsCol = {};
+  let eventsInRow = [];
+  for (let i = 0; i < events.length; i++) {
+    if ((i + 1) % 3 === 0) {
+      eventsInRow.push(events[i]);
+      eventsCol["row"] = eventsInRow;
+      eventRows.push(eventsCol);
+      eventsCol = {};
+      eventsInRow = [];
+    } else {
+      eventsInRow.push(events[i]);
     }
-  });
+  }
+  return eventRows;
+}
+router.route("/").get(async (req, res) => {
+  let events = await eventData.getAllAppEvents();
+  let eventRows = eventRows;
+  if (eventsInRow.length > 0) {
+    eventsCol["row"] = eventsInRow;
+    eventRows.push(eventsCol);
+  }
+
+  if (req.session && req.session.loggedIn) {
+    return res.render("events", {
+      title: "Browse Events",
+      user: true,
+      allEvents: eventRows,
+    });
+  } else {
+    return res.render("events", {
+      title: "Browse Events",
+      user: false,
+      allEvents: eventRows,
+    });
+  }
+});
 
 export default router;
