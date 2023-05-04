790031b7127a481824ec43cd630f7a8c54eeb949
jdobariya <jay.deep3210@gmail.com>
2023-05-03 21:35:36 -0400

    Added sort by feature

--- routes/search.js
+++ routes/search.js
@@ -1,38 +1,40 @@
 import { Router } from "express";
 import {} from "../validation.js";
-import {eventData} from '../data/index.js';
-import { getEventsInRow } from "./events.js";
+import { eventData } from "../data/index.js";
+import { changeDateFormat } from "./events.js";
 const router = Router();
-router.route("/").get((req,res)=>
-{
-return res.redirect('/events')
-})
-router.route("/").post(async(req, res) => {
-    try
-    {
-        if(!req.body.searchValue.trim())
-        {
-            return res.redirect('/events')
-        }
-  let event=await eventData.getEventByKeyword(req.body.searchValue.trim().toLowerCase())
-  let eventsRow=getEventsInRow(event);
-
-  if(req.session &&req.session.loggedIn)
-    {
-      return res.render('events',{title:"Browse Events",user:true,allEvents:eventsRow,text:`Results for ${req.body.searchValue} are`})
-     }
-    else
-    {
-    return  res.render('events',{title:"Browse Events",user:false,allEvents:eventsRow,text:`results for ${req.body.searchValue} are`})
+router.route("/").get((req, res) => {
+  return res.redirect("/events");
+});
+router.route("/").post(async (req, res) => {
+  try {
+    if (!req.body.searchValue.trim()) {
+      return res.redirect("/events");
     }
-}
-catch(e)
-{
-   
-  console.log(e)
-    return res.redirect('/events')
+    let event = await eventData.getEventByKeyword(
+      req.body.searchValue.trim().toLowerCase()
+    );
+    let eventsRow = changeDateFormat(event);
 
-}
-  });
+    if (req.session && req.session.loggedIn) {
+      return res.render("events", {
+        title: "Browse Events",
+        user: true,
+        allEvents: eventsRow,
+        text: `Results for ${req.body.searchValue} are`,
+      });
+    } else {
+      return res.render("events", {
+        title: "Browse Events",
+        user: false,
+        allEvents: eventsRow,
+        text: `results for ${req.body.searchValue} are`,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return res.redirect("/events");
+  }
+});
 
 export default router;
